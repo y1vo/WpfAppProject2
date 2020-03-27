7895d95ffbfb1167db3324a0b8618431de6ebe1c
Ilyas Zhaksybek <izhaksybek@gmail.com>
2020-03-27 23:41:00 +0600

    add two new classes Person and Data

---
+++ WpfAppProject2/Data.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppProject2
+{
+    public abstract class Data
+    {
+        public string fullname;
+        public string address;
+        public string mail;
+        public string phone;
+        public string aim;
+        public string experience;
+        public string activity;
+        public string achievement;
+        public string biography;
+        public string education;
+        public string recommendation;
+        public string age;
+        public string language;
+        public string skill;
+        public string aboutMe;
+        public string salary;
+        public string birthday;
+        public string citizenship;
+        public string relocation;
+
+        public string Fullname { get => fullname; set => fullname = value; }
+        public string Address { get => address; set => address = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Aim { get => aim; set => aim = value; }
+        public string Experience { get => experience; set => experience = value; }
+        public string Activity { get => activity; set => activity = value; }
+        public string Achievement { get => achievement; set => achievement = value; }
+        public string Biography { get => biography; set => biography = value; }
+        public string Education { get => education; set => education = value; }
+        public string Recommendation { get => recommendation; set => recommendation = value; }
+        public string Age { get => age; set => age = value; }
+        public string Language { get => language; set => language = value; }
+        public string Skill { get => skill; set => skill = value; }
+        public string AboutMe { get => aboutMe; set => aboutMe = value; }
+        public string Salary { get => salary; set => salary = value; }
+        public string Birthday { get => birthday; set => birthday = value; }
+        public string Citizenship { get => citizenship; set => citizenship = value; }
+        public string Relocation { get => relocation; set => relocation = value; }
+    }
+}
---
+++ WpfAppProject2/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppProject2
+{
+    public class Person : Data
+    {
+        private string filePath = "SaveLog.txt";
+
+        public string FilePath { get => filePath; }
+
+        public void SaveTextToLog()
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+
+            using (StreamWriter streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.WriteLine(base.Fullname);
+                streamWriter.WriteLine(base.Birthday);
+                streamWriter.WriteLine(base.Address);
+                streamWriter.WriteLine(base.Phone);
+                streamWriter.WriteLine(base.Mail);
+                streamWriter.WriteLine(base.Aim);
+                streamWriter.WriteLine(base.Age);
+                streamWriter.WriteLine(base.AboutMe);
+                streamWriter.WriteLine(base.Achievement);
+                streamWriter.WriteLine(base.Activity);
+                streamWriter.WriteLine(base.Biography);
+                streamWriter.WriteLine(base.Citizenship);
+                streamWriter.WriteLine(base.Education);
+                streamWriter.WriteLine(base.Experience);
+                streamWriter.WriteLine(base.Language);
+                streamWriter.WriteLine(base.Recommendation);
+                streamWriter.WriteLine(base.Relocation);
+                streamWriter.WriteLine(base.Salary);
+                streamWriter.WriteLine(base.Skill);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "ФИО: " + base.fullname + "Birthday: " + base.birthday + "Address: " + base.address + "Phone: " + base.phone +
+                   "Mail: " + base.mail + "Aim: " + base.aim + "Age: " + base.age + "About me: " + base.aboutMe +
+                   "Achievement: " + base.achievement + "Activity: " + base.activity + "Biography: " + base.biography +
+                   "Citizenship: " + base.citizenship + "Education: " + base.education + "Experience: " + base.experience +
+                   "Language: " + base.language + "Recommendation: " + base.recommendation + "Relocation: " + base.relocation +
+                   "Salary: " + base.salary + "Skill: " + base.skill;
+        }
+    }
+}
